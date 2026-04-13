0c28ce849adecde462fc5780e53e28030fb31fc1
jexists <jexists486@gmail.com>
2026-04-13 17:50:52 +0900

    실습

--- 04.data_collection/52_실습_3/실습03_빌보드차트100.py
+++ 04.data_collection/52_실습_3/실습03_빌보드차트100.py
@@ -10,16 +10,17 @@ page = requests.get(url)
 bs_result = BeautifulSoup(page.content,'html.parser')
 # print(bs_result)
 
-hot100 = bs_result.find_all('div', class_='chart-results-list // a-become-full-width@mobile-max')
+hot100 = bs_result.find_all('div', class_='o-chart-results-list-row-container')
 # print(hot100)
 
-print(len(hot100))
-
+# print(len(hot100))
+# title-of-a-story
 # # 확인해보기
-# info = hot100[0].find_all(   )
-# title =
-# rank =
-# artist =
-# print(rank)
-# print(title)
-# print(artist)
+info = hot100[0].find_all('li', class_='o-chart-results-list__item')
+# print(info)
+title = hot100[0].select_one('#title-of-a-story').text.strip()
+artist = hot100[0].select_one('#title-of-a-story+span').text.strip()
+rank = hot100[0].select_one('.o-chart-results-list__item span.c-label').text.strip()
+print(rank)
+print(title)
+print(artist)
