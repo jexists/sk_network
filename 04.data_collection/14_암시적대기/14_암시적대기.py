a0c89b237ee514417000933456810d255a859b90
jexists <jexists486@gmail.com>
2026-04-14 14:33:30 +0900

    대기

---
+++ 04.data_collection/14_암시적대기/14_암시적대기.py
@@ -0,0 +1,19 @@
+from selenium.webdriver import Chrome
+
+# 08_비동기방식으로_정보가_보여지는_페이지.py
+driver = Chrome()
+
+# 기상청 사이트를 브라우저에서 연결
+driver.get('https://www.weather.go.kr/w/index.do')
+
+# 암시적 대기 설정
+# 요소를 찾기 전에 최대 3초까지 기다림
+driver.implicitly_wait(3) # 페이지가 모두 로드될 때까지 최대 3초 대기
+
+# 온도정보 요소 찾음
+element = driver.find_element(by='css selector', value='#current-weather span.tmp')
+
+# 요소의 텍스트를 출력
+print(element.text)
+
+driver.close()
---
+++ 04.data_collection/15_명시적대기/15_명시적대기.py
@@ -0,0 +1,24 @@
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.support import expected_conditions as EC # 기다릴 조건들
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+driver = Chrome()
+
+# 기상청 사이트를 브라우저에서 연결
+driver.get('https://www.weather.go.kr/w/index.do')
+
+# 명시적 대기 설정
+# 최대 5초 동안 특정 요소가 나타날 때까지 기다림
+wait = WebDriverWait(driver, 5)
+
+# 조건
+element = wait.until(
+  EC.presence_of_element_located(('css selector', '#current-weather span.tmp'))
+)
+
+print(element.text)     # 요소의 텍스트를 출력
+
+driver.close()
+
